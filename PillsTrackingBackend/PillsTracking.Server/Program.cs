b5f17a7aeb225188ca1d5762c8006a3d1ef5d0a8
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-04-03 23:24:13 +0300

    Fix Swagger

--- PillsTrackingBackend/PillsTracking.Server/Program.cs
+++ PillsTrackingBackend/PillsTracking.Server/Program.cs
@@ -1,7 +1,13 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddSwaggerGen(c =>
+{
+	c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -11,8 +17,9 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1"));
 }
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
