fd2abde79c9b9642758b864a17772c10905ee6d2
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-04-07 22:30:19 +0300

    Move Identity Data folder to Data Access

---
+++ PillsTrackingBackend/PillsTracking.DataAccess/ApplicationDbContextSeed.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace PillsTracking.Server.Data
+{
+	public class ApplicationDbContextSeed
+	{
+		public static async Task SeedUsersAsync(UserManager<IdentityUser> userManager)
+		{
+			if (!userManager.Users.Any())
+			{
+				var user = new IdentityUser
+				{
+					UserName = "Admin",
+					Email = "admin@localhost.com"
+				};
+
+				await userManager.CreateAsync(user, "Pa$$w0rd");
+			}
+		}
+	}
+}
