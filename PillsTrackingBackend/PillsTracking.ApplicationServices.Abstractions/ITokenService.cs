1c48b67f3c59cda331e354201724012701007686
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-04-07 23:49:33 +0300

    Add Tokens service

---
+++ PillsTrackingBackend/PillsTracking.ApplicationServices.Abstractions/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using PillsTracking.DataAccess;
+
+namespace PillsTracking.ApplicationServices.Abstractions
+{
+	public interface ITokenService
+	{
+		string CreateToken(ApplicationUser user);
+	}
+}
