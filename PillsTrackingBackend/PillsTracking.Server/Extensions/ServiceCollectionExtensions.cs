1478fbf2b70340a4ab99f00587c803c286e14035
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-04-08 16:18:06 +0300

    Update Service extensions

--- PillsTrackingBackend/PillsTracking.Server/Extensions/ServiceCollectionExtensions.cs
+++ PillsTrackingBackend/PillsTracking.Server/Extensions/ServiceCollectionExtensions.cs
@@ -1,5 +1,8 @@
-﻿using PillsTracking.ApplicationServices;
+﻿using Microsoft.AspNetCore.Hosting;
+using PillsTracking.ApplicationServices;
 using PillsTracking.ApplicationServices.Abstractions;
+using PillsTracking.DataAccess.Abstractions;
+using PillsTracking.DataAccess.Repositories;
 
 namespace PillsTracking.Server.Extensions
 {
@@ -8,6 +11,9 @@ namespace PillsTracking.Server.Extensions
 		public static IServiceCollection RegisterApplication(this IServiceCollection services)
 		{
 			services.AddScoped<ITokenService, TokenService>();
+			services.AddScoped<IPatientService, PatientService>();
+			services.AddScoped<IPatientRepository, PatientRepository>();
+			services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 			return services;
 		}
 	}
