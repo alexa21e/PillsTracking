5f481951f2ae269af80a84ba87640e9d33520807
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-04-08 16:17:35 +0300

    Create AddPatient endpoint

--- PillsTrackingBackend/PillsTracking.Server/Controllers/PatientsController.cs
+++ PillsTrackingBackend/PillsTracking.Server/Controllers/PatientsController.cs
@@ -1,4 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
+using PillsTracking.ApplicationServices.Abstractions;
+using PillsTracking.DataObjects;
+using PillsTracking.Domain;
 
 namespace PillsTracking.Server.Controllers
 {
@@ -6,11 +9,31 @@ namespace PillsTracking.Server.Controllers
     [ApiController]
     public class PatientsController : ControllerBase
     {
+        private readonly IPatientService _patientService;
+        public PatientsController(IPatientService patientService)
+        {
+			_patientService = patientService;
+		}
+
         [HttpGet]
         public IActionResult Get()
         {
             return Ok("Get all patients");
         }
+
+        [HttpPost]
+        public async Task<ActionResult<Patient>> AddPatient([FromBody] PatientToCreateDTO patientToCreate)
+        {
+			try
+			{
+				var patient = await _patientService.AddPatient(patientToCreate);
+				return Ok(patient);
+			}
+			catch (Exception ex)
+			{
+				return BadRequest();
+			}
+		}
     }
 }
 
