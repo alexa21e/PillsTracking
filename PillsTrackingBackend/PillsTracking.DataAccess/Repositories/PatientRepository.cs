c3b69d3d17f7fa14e6ad112424905780010c9036
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-04-08 16:17:17 +0300

    Create PatientToCreateDTO and AddPatient method to PatientService

---
+++ PillsTrackingBackend/PillsTracking.DataAccess/Repositories/PatientRepository.cs
@@ -0,0 +1,20 @@
+﻿using PillsTracking.DataAccess.Abstractions;
+using PillsTracking.Domain;
+
+namespace PillsTracking.DataAccess.Repositories
+{
+    public class PatientRepository : IPatientRepository
+    {
+        private readonly PillsTrackingDbContext _dbContext;
+
+        public PatientRepository(PillsTrackingDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddPatient(Patient patient)
+        {
+            await _dbContext.Patients.AddAsync(patient);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
