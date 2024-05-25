3f9f317b8b5da8a21b52f23c4c18bb38c527f8ec
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-05-25 23:21:30 +0300

    Add authentication

---
+++ PillsTrackingFrontend/PillsTrackingMobile/app/_layout.tsx
@@ -0,0 +1,10 @@
+import { Slot } from 'expo-router';
+import { SessionProvider } from '@/context/ctx';
+
+export default function Root() {
+  return (
+    <SessionProvider>
+      <Slot />
+    </SessionProvider>
+  );
+}
