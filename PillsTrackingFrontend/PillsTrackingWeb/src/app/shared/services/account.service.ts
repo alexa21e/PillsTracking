a8b3aa185f5bf6e1055dc00c27bb72f3ea31917c
alexa21e <pirvuletu.e.alexandra@gmail.com>
2024-05-26 20:40:18 +0300

    Add landing page and fix routings

--- PillsTrackingFrontend/PillsTrackingWeb/src/app/shared/services/account.service.ts
+++ PillsTrackingFrontend/PillsTrackingWeb/src/app/shared/services/account.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ReplaySubject, map, of } from 'rxjs';
+import { BehaviorSubject, ReplaySubject, map, of } from 'rxjs';
 import { User } from '../models/user';
 import { Router } from '@angular/router';
 import { environment } from '../../../environments/environment';
@@ -13,7 +13,10 @@ export class AccountService {
 
   private currentUserSource = new ReplaySubject<User | null>(1);
   currentUser$ = this.currentUserSource.asObservable();
-
+  
+  private currentUserRoleSource = new BehaviorSubject<string | null>(null);
+  currentUserRole$ = this.currentUserRoleSource.asObservable();
+  
   constructor(private http: HttpClient,
     private router: Router) { }
 
@@ -30,6 +33,7 @@ export class AccountService {
       map((user) => {
         if(user){
           this.currentUserSource.next(user);
+          this.currentUserRoleSource.next(user.role);
           return user;
         }
         else{
@@ -47,6 +51,7 @@ export class AccountService {
       map((user) => {
         localStorage.setItem('token', user.token);
         this.currentUserSource.next(user);
+        this.currentUserRoleSource.next(user.role);
         return user;
       })
     );
@@ -57,6 +62,7 @@ export class AccountService {
       map(user => {
         localStorage.setItem('token', user.token);
         this.currentUserSource.next(user);
+        this.currentUserRoleSource.next(user.role);
         return user;
       })
     )
@@ -67,6 +73,7 @@ export class AccountService {
       map(user => {
         localStorage.setItem('token', user.token);
         this.currentUserSource.next(user);
+        this.currentUserRoleSource.next(user.role);
         return user;
       })
     )
@@ -76,5 +83,13 @@ export class AccountService {
     localStorage.removeItem('token');
     this.currentUserSource.next(null);
     this.router.navigateByUrl('/');
+  }
+
+  redirectBasedOnRole(user: User) {
+    if (user.role === 'Admin') {
+      this.router.navigate(['/admin']);
+    } else if (user.role === 'Doctor') {
+      this.router.navigate(['/doctor']);
+    }
   }
 }
